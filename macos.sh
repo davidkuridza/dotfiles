55395633dedec45cc42b435ae4467e03c0fb0160
David Kuridža <david@kuridza.si>
2017-02-06 14:03:16 +0100

    macos: add Control Strip settings

--- macos.sh
+++ macos.sh
@@ -336,6 +336,19 @@ sudo mdutil -i on / > /dev/null
 sudo mdutil -E / > /dev/null
 
 ###############################################################################
+# Control Strip                                                               #
+###############################################################################
+
+defaults write com.apple.controlstrip FullCustomized -array \
+   "com.apple.system.group.brightness" \
+   "com.apple.system.mission-control" \
+   "com.apple.system.launchpad" \
+   "com.apple.system.group.keyboard-brightness" \
+   "com.apple.system.group.media" \
+   "com.apple.system.group.volume" \
+   "com.apple.system.screen-lock"
+
+###############################################################################
 # iTerm 2                                                                     #
 ###############################################################################
 
