91fecf0c910d09a2de15cac2beaa3ed2b61a1336
David Kuridža <david@kuridza.si>
2023-10-04 11:45:31 +0200

    macos: remove redundant settings

--- macos.sh
+++ macos.sh
@@ -149,32 +149,8 @@ defaults write com.apple.dock show-recents -bool FALSE
 # Restart the Dock to apply the above settings.
 killall Dock
 
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-
-# Display emails in threaded mode, sorted by date (oldest at the top)
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
-
-# Show most recent message at the top
-defaults write com.apple.mail ConversationViewSortDescending -int 1
-
-# Disable inline attachments (just show the icons)
-defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
-
-# Enable automatic spell checking
-defaults write com.apple.mail SpellCheckingBehavior -string "InlineSpellCheckingEnabled"
-
-# Do not load remote content in messages
-defaults write com.apple.mail DisableInlineAttachmentViewing -int 1
-
 # Load new settings before rebuilding the index
 sudo killall mds > /dev/null 2>&1
-# Make sure indexing is enabled for the main volume
-sudo mdutil -i on / > /dev/null
-# Rebuild the index from scratch
-sudo mdutil -E / > /dev/null
 
 ###############################################################################
 # iTerm 2                                                                     #
